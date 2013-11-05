d394d9cdb5ad3c6ebfdb52c3c222b672400437f3
Dainius <daynewss@gmail.com>
2013-11-05 17:59:47 +0200

    Issaugo visus x ir f

--- main.cpp
+++ main.cpp
@@ -21,15 +21,20 @@ int main()
     srand(100); // srand(time(NULL));
     double a[2] = {-5, 0};
     double b[2] = {10, 15};
-    double x [2];
+    double x[2];
     double xMin[2];
+    double xAll[100][2];
     double f;
+    double fAll[100];
     double fMin = 1000000;
     for (int i=0; i<100;i++)
     {
         x[0]=fRand(a[0], b[0]);
         x[1]=fRand(a[1], b[1]);
         f = branin(&x[0]);
+        xAll[i][0]=x[0];
+        xAll[i][1]=x[1];
+        fAll[i]=f;
         if (f < fMin){
                 fMin = f;
                 xMin[0] = x[0]; xMin[1] = x[1];
@@ -38,5 +43,9 @@ int main()
 
     cout << "fMin = " << fMin << endl;
     cout << "xMin = (" << xMin[0] << ", " << xMin[1] << ")" << endl;
+    for (int i=0; i<100;i++)
+    {
+        cout <<"fAll[" << i+1 <<"]: " << fAll[i] << endl;
+    }
     return 0;
 }
